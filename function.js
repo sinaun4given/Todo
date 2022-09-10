e5d5b78c22c031565612c3c606658da8e246ccad
$imba <107941962+sinaun4given@users.noreply.github.com>
2022-09-10 23:39:29 +0430

    Update function.js

--- function.js
+++ function.js
@@ -1,39 +1,48 @@
 const toast=document.getElementById("alert");
 
  const alertsss=(msg,Option) =>{
-toast.style.right="0%"
+toast.style.right="0%";
 toast.children[0].innerHTML=msg;
 toast.children[1].addEventListener('click',()=>{
-    toast.style.right="100%"
+    toast.style.right="100%";
 });
 setTimeout(function(){
     toast.style.right="-100%"
 
 },Option.time || 3000)
- }
+ };
 
 
 const todoTitle = document.getElementById("title");
 const todoDesc = document.getElementById("desc");
 const todoMain = document.getElementById("main");
-
+ 
 
 
 /////
 
-const SavedLCTodos = localStorage.getItem("Todoslist")
+const SavedLCTodos = localStorage.getItem("Todoslist");
 
 //inja migim age     in qesmat undifind bud areye khali bar gardoon 
 const parseLCtodos = JSON.parse(SavedLCTodos) || [];
 
 
-const CreateNewTodo = (title,desc) =>{
+const CreateNewTodo = (title,desc,id,cheked) =>{
 
     //in ja az tarigh js tag html ijad kardim
 
 const listItem =document.createElement("li");
-const todoTIitleHeading =document.createElement("h3");
+listItem.id=id;
+listItem.className="list-item";
+const todoTIitleHeading =document.createElement("input");
+todoTIitleHeading.disabled=true;
+
+todoTIitleHeading.defaultValue=title;
+
+todoTIitleHeading.style.backgroundColor="orange"
+
 const todoDescPara =document.createElement("p");
+todoDescPara.style.color="white";
 todoTIitleHeading.innerHTML=title;
 todoDescPara.innerHTML=desc;
 listItem.appendChild(todoTIitleHeading);
@@ -41,14 +50,13 @@ listItem.appendChild(todoDescPara);
 
 const todoAction =`
 <div>
-<button>Del</button>
+<button >Del</button>
 <button>Edit</button>
 <button>Update</button>
 </div>
 `
 todoMain.appendChild(listItem);
 listItem.innerHTML+=todoAction;
-
 
 }
 
@@ -57,22 +65,88 @@ let saveTodo=[...parseLCtodos];
 
 
 ////
+
+//handel delet 
+
+todoMain.addEventListener("click",(e) =>{
+    if(e.target.innerText==="Del"){
+        const todoEl =e.target.parentElement.parentElement;
+        console.log(todoEl.id);
+        const filtredTodos = saveTodo.filter(
+            (item) => item.id !== Number(todoEl.id));
+            
+            localStorage.setItem("Todoslist",JSON.stringify(filtredTodos));
+             location.reload();
+     }
+
+
+     else if(e.target.innerText === "Update"){
+         //get li element
+        const todoEl =e.target.parentElement.parentElement;
+        //get our todo in localstoreg with id
+        const filtredTodo = saveTodo.filter((item) => item.id === Number(todoEl.id));
+           //update our todo in local witj id
+        const updateFiltredTodo = {...filtredTodo[0], cheked:true };
+//delet our todo from local
+        const filtredTodos =saveTodo.filter((item) => item.id !== Number(todoEl.id))
+//update local with update todo
+            const updateSaveTodos=[...filtredTodos,updateFiltredTodo]
+            todoEl.children[0].style.backgroundColor="green"
+            localStorage.setItem("Todoslist",JSON.stringify(updateSaveTodos));
+            location.reload();
+             }
+    else if(e.target.innerText === "Edit"){
+        const todoEl =e.target.parentElement.parentElement;
+        todoEl.children[0].disabled=false;
+        todoEl.children[0].select();
+
+        e.target.innerText="Save"
+        e.target.addEventListener("click", () =>{
+            
+            todoEl.children[0].disabled=true;
+
+
+            const filtredTodo = saveTodo.filter((item) => item.id === Number(todoEl.id));
+           
+            const updateFiltredTodo = {...filtredTodo[0], title:todoEl.children[0].value };
+    
+            const filtredTodos =saveTodo.filter((item) => item.id !== Number(todoEl.id))
+    
+                const updateSaveTodos=[...filtredTodos,updateFiltredTodo]
+                todoEl.children[0].style.backgroundColor="green"
+                localStorage.setItem("Todoslist",JSON.stringify(updateSaveTodos));
+
+                location.reload();
+        }
+
+        )
+
+    }
+
+  
+} );
+
+////
     
 //handle add new todo 
-saveTodo.forEach(todo=> CreateNewTodo(todo.title,todo.desc))
+saveTodo.forEach(todo=> CreateNewTodo(todo.title,todo.desc,todo.id,todo.cheked));
 export const handleCraeteNewTodo = (event) => {
     event.preventDefault();
     if(!(todoTitle.value || todoDesc.value) ) {return alertsss("لطفن جای خالی را پر بفرمایید!",{time: "5000",type:"warn"});}
     const newTodo = {
-        title: todoTitle.value,
-        desc: todoDesc.value,
+       // id:Math.floor(Math.random() * 10000000),
+       id:Date.now(), 
+       title: todoTitle.value,
+       desc: todoDesc.value,
+       cheked:false,
+       
     };
-    saveTodo.push(newTodo)
+    saveTodo.push(newTodo);
     
-    localStorage.setItem("Todoslist",JSON.stringify(saveTodo))
+    localStorage.setItem("Todoslist",JSON.stringify(saveTodo));
    
 
-CreateNewTodo(newTodo.title,newTodo.desc)
+CreateNewTodo(newTodo.title,newTodo.desc,newTodo.id)
 
 
 
